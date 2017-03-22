226c07a938d5deaf5883d38e0b3bff692a1083f1
Dag Stuan <D.Stuan@gmail.com>
2017-03-22 15:35:44 +0100

    initial demo-commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import('./js/firstEntryPoint').then((firstEntryPoint) => {
-  const FirstEntryPoint = firstEntryPoint.default;
+import FirstEntryPoint from './js/firstEntryPoint';
+import SecondEntryPoint from './js/secondEntryPoint';
 
-  ReactDOM.render(
-    <FirstEntryPoint />,
-    document.getElementById('firstEntry'),
-  );
-});
+ReactDOM.render(
+  <FirstEntryPoint />,
+  document.getElementById('firstEntry'),
+);
 
-import('./js/secondEntryPoint').then((secondEntryPoint) => {
-  const SecondEntryPoint = secondEntryPoint.default;
+ReactDOM.render(
+  <SecondEntryPoint />,
+  document.getElementById('secondEntry'),
+);
 
-  ReactDOM.render(
-    <SecondEntryPoint />,
-    document.getElementById('secondEntry'),
-  );
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import('./js/firstEntryPoint').then((firstEntryPoint) => {
+//   const FirstEntryPoint = firstEntryPoint.default;
+//
+//   ReactDOM.render(
+//     <FirstEntryPoint />,
+//     document.getElementById('firstEntry'),
+//   );
+// });
+//
+// import('./js/secondEntryPoint').then((secondEntryPoint) => {
+//   const SecondEntryPoint = secondEntryPoint.default;
+//
+//   ReactDOM.render(
+//     <SecondEntryPoint />,
+//     document.getElementById('secondEntry'),
+//   );
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,10 @@ const path = require('path');
 module.exports = {
   entry: {
     main: './src/index.js',
-    vendor: ['react'],
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        enforce: 'pre',
-        use: [
-          'eslint-loader',
-        ],
-      },
       {
         test: /.jsx?$/,
         exclude: /node_modules/,
@@ -25,34 +17,6 @@ module.exports = {
       }
     ],
   },
-  devtool: 'inline-source-map',
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      eslint: {
-        failOnError: false,
-        failOnWarning: false,
-      },
-    }),
-    // extract CSS into separate file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'main',
-      children: true,
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      },
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity
-    })
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   names: [ 'vendor', 'manifest' ]
-    // })
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
